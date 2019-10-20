784f31e86a951ab9aba3b5ea3b0ec85fb4350a3b
Xianzhe Yang <yangxianzhe123@hotmail.com>
2019-10-20 17:07:48 +1100

    Finish Login and registration and api and current location, need to be displayed on screen

--- app/src/main/java/com/capa/capa/mobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/capa/capa/mobilecomputingproject/MainActivity.java
@@ -1,14 +1,86 @@
 package com.capa.capa.mobilecomputingproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 99;
+    private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                },
+                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        TextView signUp_text = findViewById(R.id.signUp_text);
+        signUp_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistration();
+                finish();
+            }
+        });
+        emailEditText = findViewById(R.id.userEmail);
+        passwordEditText = findViewById(R.id.userPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+        Button signInBtn = findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                if (!email.isEmpty()&&!password.isEmpty()){
+                    mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            startActivity(new Intent(MainActivity.this, Weather.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void openRegistration(){
+        Intent intent = new Intent(this,Registration.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
---
+++ app/src/main/java/com/capa/capa/mobilecomputingproject/Registration.java
@@ -0,0 +1,79 @@
+package com.capa.capa.mobilecomputingproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class Registration extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText userNameEditText, userPasswordEditText,userEmailEditText;
+    String userName, userPassword, userEmail;
+    Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        TextView signIn_text = findViewById(R.id.signIn_text);
+        signIn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+                finish();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+        userNameEditText = findViewById(R.id.userName);
+        userEmailEditText = findViewById(R.id.userEmail);
+        userPasswordEditText = findViewById(R.id.userPassword);
+
+
+
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userName = userNameEditText.getText().toString();
+                userPassword = userPasswordEditText.getText().toString();
+                userEmail = userEmailEditText.getText().toString();
+                if(!userEmail.isEmpty()&&!userPassword.isEmpty()&&!userName.isEmpty()){
+                    Toast.makeText(Registration.this, "Button works fine", Toast.LENGTH_LONG).show();
+                    mAuth.createUserWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                openMenu();
+                            }else {
+                                Toast.makeText(Registration.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+    public void openLogin(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void openMenu(){
+        Intent intent = new Intent(this, Weather.class);
+        startActivity(intent);
+    }
+
+}
